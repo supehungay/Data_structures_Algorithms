d23b4552cc0733123b55137d10a52e06760acd9a
supehungay <dohung6708@gmail.com>
2023-12-01 04:35:51 +0700

    add week8

---
+++ week8/adjacencymatrix/AdjMatrix.java
@@ -0,0 +1,28 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+// @SuppressWarnings({"unchecked", "deprecation"})
+public class AdjMatrix {
+	
+	private int[][] matrix; // ma trận kề
+	private String[] v;// danh sách các đỉnh
+	private int n;// số đỉnh
+	
+	public void loadGraphFromFile(String fileName)
+	{
+	
+	}
+	
+	public  List<String> getAdjList()
+	{
+		return null;
+	}
+	
+
+	public static void main(String[] args) throws Exception {
+		FileReader readFile = new FileReader("inputFile.txt");
+	}
+
+}
---
+++ week8/graphorder/GraphDegree.java
@@ -0,0 +1,29 @@
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+@SuppressWarnings({"unchecked", "deprecation"})
+public class GraphDegree {
+	
+	private int[][] matrix;// ma trận kề
+	private String[] v;// danh sách các đỉnh
+	private int n;// số đỉnh
+	
+	public void loadGraphFromFile(String fileName)
+	{
+	
+	}
+
+	
+	public int[] getDegree()
+	{
+	
+		
+		return null;
+	}
+	
+
+
+}
